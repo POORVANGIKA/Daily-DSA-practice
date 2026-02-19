0aa0975f8877a4276e65518d44e7e28acc019d7e
Purvangika kanwar <purvangikakanwar10maabharti@gmail.com>
2026-02-19 19:05:56 +0530

    Initial commit

---
+++ first.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+class Message{
+public:
+    Message(){
+        cout<<"hello everyone here I will practice DSA and C++ programming language everyday"<<endl;
+    }
+};
+
+int main()
+{
+    Message obj; 
+    return 0;
+}
