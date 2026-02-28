162e999f175548ffdaa5eaf8f0b5cd3d0affe251
Purvangika kanwar <purvangikakanwar10maabharti@gmail.com>
2026-02-28 23:59:22 +0530

    added pattern practice

---
+++ square pattern.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i = 1; i <= n; i++) {      // loop for rows
+        for(int j = 1; j <= n; j++) {  // loop for columns
+            cout << "*";
+        }
+        cout << endl;                  // move to next line
+    }
+
+    return 0;
+}
